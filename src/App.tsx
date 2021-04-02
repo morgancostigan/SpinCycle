d29319bc669b20371ad1b089ea72dec98bbb9de7
morgancostigan <morgan.costigan@gmail.com>
2021-04-02 15:10:34 -0500

    built out initial state

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import React = require('react');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+interface IPost {
+  id: number;
+  userId?: number;
+  title: string;
+  body: string;
+};
+
+const defaultPosts:IPost[] = [];
+
+const App = () => {}
+
+const [posts, setPosts]: [IPost[], posts(IPost[]) => void] = 
+React.useState(defaultPosts);
+
+const [loading, setLoading]: [boolean, (loading: boolean) => void] = 
+React.useState<boolean>(true);
+
+const [error, setError]: [string, (error: string) => void] = 
+React.useState("");
+
+React.useEffect(() => {
+  axios.get<IPost[]>("https://jsonplaceholder.typicode.com/posts");
+}, []);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
